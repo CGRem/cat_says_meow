831f5816389b532162d578bddbd76bc3ee16c4f2
rem_w <cgrem78@yandex.ru>
2024-02-14 09:44:42 +0300

    say meow 1 time

--- objects.cpp
+++ objects.cpp
@@ -8,15 +8,19 @@ namespace objects_r
     Cat_Cl::Cat_Cl(){
         cat_breed_Str = "none";
     }
+
     void Cat_Cl::says_meow(){
         std::cout << "Кот говорит МЯУ" << std::endl;
     }
+
     void Cat_Cl::set_color(std::string *color_Str){
         color_Str_ = *color_Str;
     }
+
     void Cat_Cl::get_color(){
         std::cout << "Кот цвета " << color_Str_ << std::endl;
     }
+
     void Cat_Cl::cat_says_time(){
         std::cout << "Кот говорит сейчас " << func_r::unix_time_now_Tt() << " секунд" << std::endl;
     }
