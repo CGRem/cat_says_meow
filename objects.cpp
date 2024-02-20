613284ca343fd7a6e0ef8ab817d79679af358d19
CGRem <125661027+CGRem@users.noreply.github.com>
2024-02-20 19:28:47 +0300

    UTF-8

--- objects.cpp
+++ objects.cpp
@@ -11,17 +11,15 @@ namespace objects_r
     }
 
     void Cat_Cl::says_meow(){
-        std::cout << "Кот говорит МЯУ" << std::endl;
+        std::cout << "Кот говорит МЯУ " << std::endl;
     }
 
     void Cat_Cl::set_color(std::string const& color_Str){
         color_Str_ = color_Str;
     }
-
     void Cat_Cl::get_color(){
         std::cout << "Кот цвета " << color_Str_ << std::endl;
     }
-
     void Cat_Cl::cat_says_time(){
         std::cout << "Кот говорит сейчас " << func_r::unix_time_now_Tt() << " секунд" << std::endl;
     }
